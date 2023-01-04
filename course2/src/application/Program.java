224122ae6c4c33e175655e4ab392b0eb6ece8ed3
Weslley Paes <95313382+Weslleypaes@users.noreply.github.com>
2023-01-04 11:16:47 -0300

    Add files via upload

--- course2/src/application/Program.java
+++ course2/src/application/Program.java
@@ -19,18 +19,26 @@ public class Program {
 		System.out.print("Price: ");
 		double price = sc.nextDouble();
 
-		System.out.print("Quantity in stock: ");
-		int quantity = sc.nextInt();
+//		System.out.print("Quantity in stock: ");
+//		int quantity = sc.nextInt();
 
-		Product product = new Product(name, price, quantity);
+		Product product = new Product(name, price);
+		
+		product.setName("Computer");
+		System.out.println("Updated name: "+ product.getName());
+		
+		product.setPrice(700);
+		System.out.println("Updated price: "+ product.getPrice());
+		System.out.println("Updated quantity: "+ product.getQuantity());
 		
 		System.out.println();
 		System.out.println("Product data: " + product);
 
 		System.out.println();
 		System.out.println("Enter the number of products to be added in stock: ");
-		quantity = sc.nextInt();
+		int quantity = sc.nextInt();
 		product.addProducts(quantity);
+		
 
 		System.out.println();
 		System.out.println("Updated data: " + product);
--- course2/src/entities/Product.java
+++ course2/src/entities/Product.java
@@ -2,20 +2,53 @@ package entities;
 
 public class Product {
 //Atributos da classe
-	public String name;
-	public double price;
-	public int quantity;
-
+	private String name;
+	private double price;
+	private int quantity;
+	
+//Construtores	
+	
+	public Product() {
+	}
+	
 	public Product(String name, double price, int quantity) {
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
 	}
 	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	//Métodos
+	
 	public double totalValueInStock() {
 		return price * quantity;
 	}
 	
+
 	public void addProducts(int quantity) {
 		 this.quantity += quantity;
 	}
